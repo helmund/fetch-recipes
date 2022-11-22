bfe5784e94d9671ad9e9ae84cabcec1cb91cb0a0
Sascha Helmund <sascha.helmund@appsfactory.de>
2022-11-22 12:05:27 +0100

    add script to handle recipes

---
+++ handerecipes.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+let fileName;
+
+if (Object.keys(args).length === 0) {
+  fileName = "recipes-500";
+} else {
+  fileName = args[0];
+}
+
+fs.unlink(`data/${fileName}-output.json`, (err) => {
+  if (err) {
+    console.log("new File, nice");
+    return;
+  }
+});
+
+let rawData = fs.readFileSync(`data/${fileName}.json`);
+let data = JSON.parse(rawData);
+
+let recipes = [];
+
+let i = 0;
+data.items.forEach((element) => {
+  // filter out all recipes for the thermomix
+  if (element.author && element.author == "thermomix") return;
+  if (element.comment && element.comment.includes("thermomix")) return;
+
+  i++;
+  console.log(i);
+  console.log(element.slug);
+  recipes.push(element);
+});
+
+let jsonContent = JSON.stringify(recipes);
+
+fs.writeFile(
+  `data/${fileName}-output.json`,
+  jsonContent,
+  "utf8",
+  function (err) {
+    if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+    }
+
+    console.log("JSON file has been saved.");
+  }
+);
